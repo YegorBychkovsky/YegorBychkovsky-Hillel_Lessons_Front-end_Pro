28b7281b57d7d8e406aad6e3bb5da4b1d279a743
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-10-06 19:12:22 +0300

    Lesson-03

---
+++ lesson-03/index.js
@@ -0,0 +1,60 @@
+//ДЗ №3
+
+
+// var firstNumber = Number(prompt('Введите число', '' ));
+// var secondNumber = Number(prompt('Введите число', '' ));
+
+
+// alert(`Результаты математических операций :
+//  ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}
+//  ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}
+//  ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}
+//  ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+
+
+// ДЗ №4
+
+
+// var operation = prompt('Какую мат. операцию хотите совершить (add, sub, mult, div)?', '')
+// var firstNumber = Number(prompt('Введите число', '' ));
+// var secondNumber = Number(prompt('Введите число', '' ));
+ 
+// if (operation === 'add') {
+//     alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+
+// } else if (operation === 'sub') {
+//     alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+
+// } else if (operation === 'mult') {
+//     alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+
+// } else if (operation === 'div') {
+//     alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+
+// } else if (operation === null) {
+//     alert( 'Отменено' );
+// } else {
+//     alert(`operation incorect`);
+// }
+
+
+
+// ДЗ №5
+
+
+// var hour = Number(prompt('Введите кол-во часов', '' ));
+
+// var secondsInHours = hour * 3600;
+
+// alert(secondsInHours)
+
+
+
+// ДЗ №6
+
+
+var firstNumber = Number(prompt('Введите число', '' ));
+var secondNumber = Number(prompt('Введите число', '' ));
+var thirdNumber = Number(prompt('Введите число', '' ));
+
+alert(`Среднее врефметические число = ${(firstNumber + secondNumber + thirdNumber) / 3}`)
