0ff2e38907a4f66798f887083b3e15d6d1edcc00
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-10-15 01:10:33 +0300

    Lesson-05

---
+++ Lesson-05/Task-11_10.js
@@ -0,0 +1,56 @@
+let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+
+
+//Сделал двумя способами, второй мне нравится сильно больше.
+
+
+//    ПЕРВЫЙ
+
+
+// let a = arr.filter((elem) => {
+//     return elem > 0;
+// })
+// a.sort((a, b) => a - b);
+// a.find((elem) => {
+//     return elem > 0;
+// })
+// console.log(
+//     a.findLast((elem) => {
+//         return elem > 0;
+//     })
+// );
+
+// let b = 0;
+
+// for (const key in arr) {
+//     if (arr[key] === 76) {
+
+//     }
+//     for (let i = 0; i < 1; i++) {
+//         arr[key] = 0;
+//         console.log(` ${b} : ${arr[key]}`);
+//         b++
+//     }
+// }
+
+
+//   ВТОРОЙ
+
+
+// let max = arr[0]
+// let maxIndex = 0
+
+// for (i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         max = arr[i];
+//         maxIndex = i
+//     }
+// }
+
+// for (i = 0; i < arr.length; i++) {
+//     if(arr[i] != max) {
+//         arr[i] = 0;
+//     }
+// }
+
+// console.log(arr);
