5d0fca21ae5238f2cbaad76ffbce120add46ff66
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-10-18 22:03:37 +0300

    new

--- Lesson-06/Task-14.js
+++ Lesson-06/Task-14.js
@@ -1,17 +1,17 @@
-// const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-// let sum = 'Результат:'
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+let sum = 'Результат:'
 
-// function getRandomInt(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min) + min);
-//   }
-// function generateKey (a, b) {
-//     for (i = 0; i < a; i++) {
-//         let random = Math.random()*10*4
-//         d = b[Math.ceil(getRandomInt(i, random))]
-//         sum += d
-//     }
-// }
-// generateKey(6, characters)
-// console.log(sum);
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+function generateKey (a, b) {
+    for (i = 0; i < a; i++) {
+        let random = Math.random()*10*3
+        d = b[Math.ceil(getRandomInt(i, random))]
+        sum += d
+    }
+}
+generateKey(36, characters)
+console.log(sum);
