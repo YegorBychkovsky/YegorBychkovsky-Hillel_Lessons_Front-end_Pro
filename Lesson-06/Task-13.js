454180ef1211a63e330d9bb446b2185924f02fd7
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-10-18 21:52:45 +0300

    Lesson-06

---
+++ Lesson-06/Task-13.js
@@ -0,0 +1,21 @@
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// №1
+// function removeElement (a, b) {
+//     a.splice(b-1, 1)
+// }
+// removeElement(arr, 5)
+// console.log(arr);
+// №2
+// function deleteEl(a, b) {
+//     delete a[b]
+// }
+// deleteEl(arr, 2)
+// console.log(arr);
+// №3
+// let b = +prompt()
+// var newArr = arr.filter(
+//     function(a) {
+//         return a !==b;
+//     }
+// )
+// console.log(newArr); 
---
+++ Lesson-06/Task-15_2.js
@@ -0,0 +1,28 @@
+// let x = +prompt('')
+// let znak = prompt('')
+// let y = +prompt('')
+
+// function doMath(x, znak, y) {
+//     if (znak === '+') {
+//         return math = `${x} + ${y} = ${x + y}`
+//     } if (znak === '-') {
+//         return math = `${x} - ${y} = ${x - y}`
+//     } if (znak === '*') {
+//         return math = `${x} * ${y} = ${x * y}`
+//     } if (znak === '/') {
+//         return math = `${x} / ${y} = ${x / y}`
+//     } if (znak === '^') {
+//         return math = `${x} ^ ${y} = ${x ** y}`
+//     } 
+    // if (znak === '%') {
+    //     return math = `${x} % ${y} = ${x % y}`  //для вычесления остатка от деления
+    // } 
+//     if (znak === '%') {
+//         return math = `${x} % ${y} = ${x / 100 * y}` //для вычесления процента от чесла
+//     }
+    
+// }
+
+// console.log(doMath(x, znak, y));
+
+
