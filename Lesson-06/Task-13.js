6398663e7e270264429377d9a28d9cd019b6ad39
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-10-20 20:16:00 +0300

    New Task-13

--- Lesson-06/Task-13.js
+++ Lesson-06/Task-13.js
@@ -1,21 +1,18 @@
-// const arr = [1, 2, 3, 4, 5, 6, 7];
-// №1
-// function removeElement (a, b) {
-//     a.splice(b-1, 1)
-// }
-// removeElement(arr, 5)
-// console.log(arr);
-// №2
-// function deleteEl(a, b) {
-//     delete a[b]
-// }
-// deleteEl(arr, 2)
-// console.log(arr);
-// №3
-// let b = +prompt()
-// var newArr = arr.filter(
-//     function(a) {
-//         return a !==b;
-//     }
-// )
-// console.log(newArr); 
+const arr = [1, 2, 65, 23, 'ttt', 94, 0, 14587, 9, undefined, -485, 784];
+function removeElement (a, b) {
+    c = a.indexOf(b);
+    if (a.indexOf(b) === -1) {
+        return
+    }
+    a.splice(c, 1);
+}
+removeElement(arr, 'erwb');
+console.log(arr);
+
+
+
+// console.log(removeElement([5 ,38, 67, 43, 42, -5, 0, 81], 42)) // [5 ,38, 67, 43, -5, 0, 81];
+// console.log(removeElement([5 ,38, 67, 43, 42, -5, 0, 81], 38)) // [5, 67, 43, 42, -5, 0, 81];
+// console.log(removeElement([5 ,38, 67, 43, 42, -5, 0, 81], 5)) // [38 , 67, 43, 42, -5, 0, 81];
+// console.log(removeElement([5, 4, 3, 2, 1], 4)) // [4, 3, 2, 1];
+// console.log(removeElement([5, 4, 3, 2, 1], 10)) // [5, 4, 3, 2, 1];
