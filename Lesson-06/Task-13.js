3c3ed5b2d3f1133f63372749a5d88a26a548cfde
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-10-20 21:46:54 +0300

    v

--- Lesson-06/Task-13.js
+++ Lesson-06/Task-13.js
@@ -1,18 +1,10 @@
-const arr = [1, 2, 65, 23, 'ttt', 94, 0, 14587, 9, undefined, -485, 784];
-function removeElement (a, b) {
-    c = a.indexOf(b);
-    if (a.indexOf(b) === -1) {
-        return
-    }
-    a.splice(c, 1);
-}
-removeElement(arr, 'erwb');
-console.log(arr);
-
-
-
-// console.log(removeElement([5 ,38, 67, 43, 42, -5, 0, 81], 42)) // [5 ,38, 67, 43, -5, 0, 81];
-// console.log(removeElement([5 ,38, 67, 43, 42, -5, 0, 81], 38)) // [5, 67, 43, 42, -5, 0, 81];
-// console.log(removeElement([5 ,38, 67, 43, 42, -5, 0, 81], 5)) // [38 , 67, 43, 42, -5, 0, 81];
-// console.log(removeElement([5, 4, 3, 2, 1], 4)) // [4, 3, 2, 1];
-// console.log(removeElement([5, 4, 3, 2, 1], 10)) // [5, 4, 3, 2, 1];
+// const arr = [1, 2, 65, 23, 'ttt', 94, 0, 14587, 9, undefined, -485, 784];
+// function removeElement (a, b) {
+//     c = a.indexOf(b);
+//     if (a.indexOf(b) === -1) {
+//         return
+//     }
+//     a.splice(c, 1);
+// }
+// removeElement(arr, 'erwb');
+// console.log(arr);
--- Lesson-06/Task-14.js
+++ Lesson-06/Task-14.js
@@ -4,11 +4,11 @@ let sum = 'Результат:'
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.ceil(Math.random() * (max - min) + min);
   }
 function generateKey (a, b) {
     for (i = 0; i < a; i++) {
-        let random = Math.random()*10*3
+        let random = Math.random()*characters.length;
         d = b[Math.ceil(getRandomInt(i, random))]
         sum += d
     }
