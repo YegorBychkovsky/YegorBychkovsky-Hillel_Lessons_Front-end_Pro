54c96790c56e1f5ca20f45a6a764c4f65fd4f11e
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-10-25 22:07:28 +0300

    New Task-18

--- Task-18/index.js
+++ Task-18/index.js
@@ -1,24 +1,16 @@
 let ladder = {
-    step: 0,
-    up() {
-      this.step++;
-      return this;
-    },
-    down() {
-      this.step--;
-      return this;
-    },
-    showStep() {
-      alert( this.step );
-      return this;
-    }
-  };
-
-
-
-// ladder.up().up().down().
-
-// ladder.up();
-// ladder.up();
-// ladder.down();
-ladder.down().up().up().down().up().up().up().up().showStep(); // 1
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    alert( this.step );
+    return this;
+  }
+};
+ladder.down().up().up().down().up().up().up().up().showStep();
