0f06ba152893e39fc19185ca50987426d6bcf9b3
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-11-08 00:58:44 +0200

    Task-22

---
+++ Task-22/index.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+const tbl = document.createElement("table");
+container.appendChild(tbl)
+
+var num = 1
+
+for (let i = 0; i < 10; i++) {
+    const tr = document.createElement("tr");
+    tbl.appendChild(tr);
+    for (let j = 0; j < 10; j++) {
+        const tdText = document.createTextNode(num++);
+        const td = document.createElement("td");
+        tr.appendChild(td);
+        td.appendChild(tdText);
+    }
+}
