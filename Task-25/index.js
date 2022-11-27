f60e0763f3b837290018b953ec98d315b5beea4e
YegorBychkovsky <106180465+YegorBychkovsky@users.noreply.github.com>
2022-11-28 01:20:57 +0200

    Delete Task-25 directory

--- Task-25/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('click', (event) => {
-    if (event.target.tagName === 'LABEL') {
-        const block = event.target.closest('.block')
-        const input = block.querySelector('input')
-        input.value++
-    } else {
-        return
-    }
-}) 
