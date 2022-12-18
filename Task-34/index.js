4effc0453e1363cb063a09584ce9ea3d890f75a3
YegorBychkovsky <egorbychkovsky@gmail.com>
2022-12-18 13:01:30 +0200

    Task-34

--- Task-34/index.js
+++ Task-34/index.js
@@ -66,6 +66,7 @@ function getWeather(city) {
                 }
                 return resolve(json)
             })
+            
             .catch((err) => {
                 reject(err);
             })
