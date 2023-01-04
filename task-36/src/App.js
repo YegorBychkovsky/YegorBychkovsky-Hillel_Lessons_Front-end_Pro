ec62eac50075b225f1c52cb52785e7780596aaf2
Yegor Bychkovsky <egorbychkovsky@gmail.com>
2023-01-04 19:01:52 +0200

    Task-36

---
+++ task-36/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Smile from "./components/Smile";
+
+function App() {
+  
+  return (
+    <main className="container">
+      <Smile name = "🥰" initialValue = {0}/>
+      <Smile name = "🥹" initialValue = {0}/>
+      <Smile name = "🙃" initialValue = {0}/>
+      <Smile name = "😇" initialValue = {0}/>
+    </main>
+  );
+}
+
+export default App;
