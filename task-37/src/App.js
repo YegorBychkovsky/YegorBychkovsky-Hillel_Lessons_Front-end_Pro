850f913a4401bf2eecdf2a1220a263d2b3d8fd94
Yegor Bychkovsky <egorbychkovsky@gmail.com>
2023-01-10 17:30:32 +0200

    new commit

--- task-37/src/App.js
+++ task-37/src/App.js
@@ -1,26 +1,33 @@
 import './App.css';
 import {ToDoBlock} from "./components/ToDoBlock";
 import {InputPlus} from "./components/InputPlus";
-import { useState } from 'react';
+import { useState , useCallback } from 'react';
 
 export const generateId = () => (
   Math.random().toString(16).slice(2) + new Date().getTime().toString(36)
 );
-
 function App() {
-  const [ tasks, setTask] = useState([]);
-console.log(tasks);
-
-
-function compFunc(a, b) {
-  if ( a.statusActive = false &&  b.statusActive != true  ) {
+  const handelFunc = useCallback((a, b) => {
+    if ( a.statusActive < b.statusActive ) {
       return -1;
-  } else if ( a.statusActive != false && b.statusActive != true  ) {
-      return 1;
-  } else {
-      return 0;
-  }
-}
+    } else if (a.statusActive > b.statusActive) {
+        return 1;
+    } else {
+        return 0;
+    }
+  })
+  const onAdd = useCallback((title)=>{
+    setTask([
+      {
+        id: generateId(),
+        title,
+        statusActive: false
+      },
+      ...tasks
+    ])
+  })
+  const [ tasks, setTask] = useState([]);
+
 
 return (
   <main className='container'>
@@ -29,16 +36,7 @@ return (
       </h1>
       <div className='input'>
         <InputPlus
-          onAdd={(title)=>{
-            setTask([
-              {
-                id: generateId(),
-                title,
-                statusActive: false
-              },
-              ...tasks
-          ])
-          }}
+          onAdd={onAdd}
         />
       </div>
       <div className='titleSection'>
@@ -51,15 +49,7 @@ return (
             addStatusFunc = {(newStatus)=>{
                 task.statusActive = !newStatus
             }}
-            compFunc = {(a, b) => {
-              if ( a.statusActive < b.statusActive ) {
-                return -1;
-              } else if (a.statusActive > b.statusActive) {
-                  return 1;
-              } else {
-                  return 0;
-              }
-            }}
+            compFunc = {handelFunc}
           />
         ))}
       </div>
