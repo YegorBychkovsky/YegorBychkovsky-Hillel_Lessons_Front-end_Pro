2a5ff0fed146f54b530e8ec87d1d682e047b67e6
Yegor Bychkovsky <egorbychkovsky@gmail.com>
2023-01-18 13:18:48 +0200

    new commit

--- task-38/src/App.jsx
+++ task-38/src/App.jsx
@@ -1,117 +1,105 @@
 import './App.css';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { ContactBlock } from "./components/ContactBlock/ContactBlock";
 import 'antd/dist/reset.css';
 import { Button, Space } from 'antd';
 import { FormContact } from './components/form/Form';
-import { InputContact } from './components/InputContact';
 
 
 const generateId = () => (
   Math.random().toString(16).slice(2) + new Date().getTime().toString(36)
 );
 
+
 function App({
 }) {
-  const [contacts, setContact] = useState([]);
-  return (
-    <div className='wrapper'>
-      <div className="addBtn">
-        <Button
-          type="primary"
-          onClick={() => {
-            document.getElementById('newFrom').style = { display: 'block' }
-          }}
-        >Add New Contact
-        </Button>
-        <div
-          id='newFrom'
-          style={{ display: 'none' }}
-        >
-          <FormContact
-            onAdd={(name, surname, phone) => {
-              setContact([
-                ...contacts,
-                {
-                  id: generateId(),
-                  name,
-                  surname,
-                  phone
-                },
-              ])
+  const [contacts, setContact] = useState([])
+
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (response === "404") {
+          console.log("Error 404");
+        }
+        return response.json()
+      })
+      .then((result) => {
+        console.log(1);
+        setContact(result)
+      })
+  }, [])
+  {
+    console.log(contacts);
+    return (
+      <div className='wrapper'>
+        <div className="addBtn">
+          <Button
+            type="primary"
+            onClick={() => {
+              document.getElementById('newFrom').style = { display: 'block' }
             }}
-          />
+          >Add New Contact
+          </Button>
+          <div
+            id='newFrom'
+            style={{ display: 'none' }}
+          >
+            <FormContact
+              onAdd={(name, surname, phone) => {
+                console.log(surname);
+                setContact([
+                  ...contacts,
+                  {
+                    id: generateId(),
+                    name: `${name} ${surname}`,
+                    phone
+                  },
+                ])
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <div className="topBlock">
-        <div className="name">
-          <h2>
-            Name
+        <div className="topBlock">
+          <div className="name">
+            <h2>
+              Name
 
-          </h2>
+            </h2>
+          </div>
+          <div className="surname">
+            <h2>
+              Surname
+            </h2>
+          </div>
+          <div className="phone">
+            <h2>
+              Phone
+            </h2>
+          </div>
+          <div className="deleteBtn">
+          </div>
         </div>
-        <div className="surname">
-          <h2>
-            Surname
-          </h2>
-        </div>
-        <div className="phone">
-          <h2>
-            Phone
-          </h2>
-        </div>
-        <div className="deleteBtn">
-        </div>
-      </div>
-      <ContactBlock
-        number={0}
-      />
-      <ContactBlock
-        number={1}
-      />
-      <ContactBlock
-        number={2}
-      />
-
-      <ContactBlock
-        number={3}
-      />
-      <ContactBlock
-        number={4}
-      />
-      <ContactBlock
-        number={5}
-      />
-
-      <ContactBlock
-        number={6}
-      />
-      <ContactBlock
-        number={7}
-      />
-      <ContactBlock
-        number={8}
-      />
-
-      <ContactBlock
-        number={9}
-      />
-      <section className='section'>
         {contacts.map((contact) => (
-          <InputContact
-            key={contact.id}
+          console.log(contact.name),
+          <ContactBlock
+            key={contact.phone}
             id={contact.id}
-            name={contact.name}
-            surname={contact.surname}
+            name={contact.name.split(' ')[0]}
+            surname={contact.name.split(' ')[1]}
             phone={contact.phone}
             onRemove={(idNumber) => {
-              setContact(contacts.filter((task) => task.id !== idNumber))
+              setContact(contacts.filter((contact) => contact.id !== idNumber))
             }}
           />
         ))}
-      </section>
-    </div>
-  );
+      </div >
+    );
+  }
+
+
+
+
 }
 
 export default App;
