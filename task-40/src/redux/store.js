332c4e203298f58365471258ad0ce4cd5bbd8f81
YegorBychkovsky <106180465+YegorBychkovsky@users.noreply.github.com>
2023-02-02 19:34:40 +0200

    Delete store.js

--- task-40/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./todos/todoSlice";
-
-export const store = configureStore({
-	reducer: {
-		counter: counterReducer,
-	},
-});
