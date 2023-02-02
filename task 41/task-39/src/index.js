e187103d2470cdb3df6d7c506b1a6024c80f330b
YegorBychkovsky <106180465+YegorBychkovsky@users.noreply.github.com>
2023-02-02 19:13:52 +0200

    task-41

---
+++ task 41/task-39/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Users } from './pages/Users';
+import { Albums } from './pages/Albums';
+import { Photos } from './pages/Photos';
+
+import '@picocss/pico';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Users />,
+  },
+  {
+    path: '/albums/:userId',
+    element: <Albums />,
+  },
+  {
+    path: '/albums/:userId/photos/:albumId',
+    element: <Photos />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>,
+);
